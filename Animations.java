01d3cca0aa5671e6e66095ec44a759a99ccfca43
StephenSong666 <zsong26@sandomenico.org>
2025-01-13 14:39:03 -0500

    5.2done

---
+++ Animations.java
@@ -0,0 +1,17 @@
+import greenfoot.*;
+/**
+ * Author: Stephen
+ * Date: 1.13.2025
+ */
+public class Animations
+{
+    public static void wobble(Card[] cards)
+    {
+        Greenfoot.playSound("wobble.wav");
+    }
+
+    public static void slideAndTurn(Card[] cards)
+    {
+        Greenfoot.playSound("swoosh.wav");
+    }
+}
